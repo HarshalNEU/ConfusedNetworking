db742d161c5293a81ef34a9740c3b9fa0943c3c2
HarshalNEU <neelkamal.h@husky.neu.edu>
2019-02-03 08:41:47 -0500

    initial commit

---
+++ ConfusedNetworking/ConfusedNetworking.h
@@ -0,0 +1,19 @@
+//
+//  ConfusedNetworking.h
+//  ConfusedNetworking
+//
+//  Created by Harshal Neelkamal on 2/3/19.
+//  Copyright © 2019 BrightApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConfusedNetworking.
+FOUNDATION_EXPORT double ConfusedNetworkingVersionNumber;
+
+//! Project version string for ConfusedNetworking.
+FOUNDATION_EXPORT const unsigned char ConfusedNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfusedNetworking/PublicHeader.h>
+
+
