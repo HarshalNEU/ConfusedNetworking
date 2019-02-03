9df3b75d8c793e4cfc5462f9e098b6521bfb43d7
HarshalNEU <neelkamal.h@husky.neu.edu>
2019-02-03 09:30:45 -0500

    status checked and its working

---
+++ ConfusedNetworking/StartupTest.swift
@@ -0,0 +1,22 @@
+//
+//  StartupTest.swift
+//  ConfusedNetworking
+//
+//  Created by Harshal Neelkamal on 2/3/19.
+//  Copyright © 2019 BrightApps. All rights reserved.
+//
+
+import Foundation
+
+public class StartUpTest {
+    
+    public init() {
+        print("Startup test initialized")
+    }
+    
+    public func testFuncToVerify(a:Int, b:Int) -> Int{
+        return a+b
+    }
+}
+
+
